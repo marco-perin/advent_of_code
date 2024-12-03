cd32a11585917039af441a6a6cd31a2ead8289f9
marco-perin <30049458+marco-perin@users.noreply.github.com>
2024-12-03 16:38:33 +0100

    Initial; 2024 commit

---
+++ 2024/rust_aoc_2024/README.md
@@ -0,0 +1,3 @@
+# 2024 AOC challenge
+
+This year's AOC will be useful to test out some rust programming maybe
---
+++ 2024/rust_aoc_2024/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Reading file...");
+
+    read_file();
+}
+
+fn read_file(){
+   // Read file and append to list 
+}
